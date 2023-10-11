352731de9dcf71d4336814eefd6b53159498f263
Alexei Vinnichek <alexei.vinnichek@mail.ru>
2023-10-11 23:09:56 +0300

    Button submit works with validation

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const registerToggleButton = document.getElementById('register-btn');
 const loginForm = document.getElementById('log-in-form');
 const registerForm = document.getElementById('register-form');
 
+//variebles for clearing input fields
+const loginUserName = document.getElementById('login-username');
+const loginUserNameError = document.getElementById('log-username-error');
+
 function chooseLoginForm(){
     registerToggleButton.style.color = 'rgb(0, 0, 0)';
     registerToggleButton.style.background = 'transparent';
@@ -10,6 +14,7 @@ function chooseLoginForm(){
     loginToggleButton.style.background = 'linear-gradient(to right, rgb(51, 106, 195), rgb(52, 199, 52))';
     registerForm.style.display = 'none';
     loginForm.style.display = 'block';
+
 }
 
 function chooseRegisterForm(){
@@ -19,4 +24,7 @@ function chooseRegisterForm(){
     loginToggleButton.style.background = 'white';
     loginForm.style.display = 'none';
     registerForm.style.display = 'block'
+    
+    //try to clear input fields by toggling button Log In and Register
+    loginUserName.value = '';
 }
--- validationForm.js
+++ validationForm.js
@@ -1,14 +1,13 @@
-const loginUsernameError = document.getElementById('log-username-error');
-const loginPasswordError = document.getElementById('log-password-error');
-const regUsernameError = document.getElementById('reg-username-error');
-const regPhoneError = document.getElementById('reg-phone-error');
-const regPasswordError = document.getElementById('reg-password-error');
+"use strict"
 
-
+function addCheckIcon(tagname){
+    tagname.innerHTML = '<i class="fas fa-check-circle"></i>';
+}
 
 function validateUserName(tagId, errorId){
     let tagForError = document.getElementById(errorId);
     let name = document.getElementById(tagId).value;
+
     if(name.length == 0){
         tagForError.innerHTML = 'Name is required!';
         return false;
@@ -18,32 +17,39 @@ function validateUserName(tagId, errorId){
         return false;
     }
 
-    tagForError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    addCheckIcon(tagForError);
     return true;
 }
 
 function validatePhone(){
+    const regPhoneError = document.getElementById('reg-phone-error');
     let telNumber = document.getElementById('reg-phone').value;
+
     if(telNumber.length == 0){
         regPhoneError.innerHTML = 'Phone number is required!';
-        return false;
-    }
-    if(!telNumber.match(/^[\d]{1,12}$/)){
-        regPhoneError.innerHTML = 'Only digits, please!';
         return false;
     }
     if(telNumber.length < 12){
         regPhoneError.innerHTML = 'Number should be longer!';
         return false;
     }
-    
-    regPhoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    if(telNumber.length > 12){
+        regPhoneError.innerHTML = 'Number should be shorter!';
+        return false;
+    }
+    if(!telNumber.match(/^[\d]{1,12}$/)){
+        regPhoneError.innerHTML = 'Only digits, please!';
+        return false;
+    }
+
+    addCheckIcon(regPhoneError);
     return true;
 }
 
 function validatePassword(tagId, errorId){
     let password = document.getElementById(tagId).value;
     let tagForError = document.getElementById(errorId);
+
     if(password.length == 0){
         tagForError.innerHTML = 'Password is required!';
         return false;
@@ -52,7 +58,23 @@ function validatePassword(tagId, errorId){
         tagForError.innerHTML = 'Incorrect password';
         return false;
     }
-    tagForError.innerHTML = '<i class="fas fa-check-circle"></i>';
+
+    addCheckIcon(tagForError);
     return true;
 
+}
+
+function validateLoginForm(){
+    if(!validateUserName('login-username', 'log-username-error') ||
+        !validatePassword('login-password', 'log-password-error')){
+            return false;
+        }
+}
+
+function validateRegisterForm(){
+    if(!validateUserName('reg-username', 'reg-username-error') ||
+        !validatePassword('reg-password', 'reg-password-error')||
+        !validatePhone()){
+            return false;
+        }
 }
